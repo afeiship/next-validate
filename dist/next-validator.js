7c182cd4246d38cd545a78a996e4ab16a40aa60f
afeiship <1290657123@qq.com>
2017-06-03 17:37:10 +0800

    update

--- dist/next-validator.js
+++ dist/next-validator.js
@@ -2,8 +2,9 @@
 
   var global = window || this;
 
-  var emailRE=/^([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/;
-  var mobileRE=/^1[34578]\d{9}$/;
+  var emailRE =/^([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/;
+  var mobileRE =/^1[34578]\d{9}$/;
+  var idCardRE = /(^\d{15}$)|(^\d{17}([0-9]|X)$)/i;
 
   var nx = global.nx || require('next-js-core2');
   var Validator = nx.declare('nx.Validator', {
@@ -27,6 +28,9 @@
         var value = String(inValue);
         var len = value.length;
         return len>inStart && len<inEnd;
+      },
+      isIdCard: function(inValue){
+        return idCardRE.test(inValue);
       }
     }
   });
