f1e11195f278de23df549b633ad34dd9c6f9384e
afeiship <1290657123@qq.com>
2017-06-03 17:49:12 +0800

    formar

--- dist/next-validator.js
+++ dist/next-validator.js
@@ -9,22 +9,22 @@
   var nx = global.nx || require('next-js-core2');
   var Validator = nx.declare('nx.Validator', {
     statics:{
-      isEmpty:function(inValue){
+      isEmpty: function(inValue){
         return !String(inValue).trim();
       },
-      isNumber:function(inValue){
+      isNumber: function(inValue){
         if(!isNaN(inValue)){
           return nx.isNumber( parseFloat(inValue) );
         }
         return false;
       },
-      isEmail:function(inValue){
+      isEmail: function(inValue){
         return emailRE.test(inValue);
       },
-      isMobile:function(inValue){
+      isMobile: function(inValue){
         return mobileRE.test(inValue);
       },
-      isInRange:function(inValue,inData){
+      isInRange: function(inValue,inData){
         var value = String(inValue);
         var len = value.length;
         return len>inData.start && len<inData.end;
